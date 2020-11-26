c1e47d04cb0f6b6549c6b57cb33ecd1cb3d6aef9
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-11-26 18:55:22 +0100

    first commit

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/front", name="front")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/", name="home")
+     */
+
+    public function home() {
+        return $this->render('front/home.html.twig');
+    }
+
+
+    /**
+     * @Route("front/12", name="front_show")
+     */
+
+    public function show() {
+        return $this->render('front/show.html.twig');
+    }
+}
